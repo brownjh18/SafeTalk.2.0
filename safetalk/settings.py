1898cd0a88aa0e291c346f6cf8231a65eb55667d
brownjh18 <brownjh18@gmail.com>
2025-10-21 15:12:25 +0300

    Remove tailwind app reference - not installed

--- safetalk/settings.py
+++ safetalk/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.facebook',
     # 'djcelery_email',  # Commented out due to package availability issues
-    'tailwind',
+    # 'tailwind',  # Commented out - not installed
 ]
 
 MIDDLEWARE = [
